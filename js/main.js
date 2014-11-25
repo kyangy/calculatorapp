4613894eebb816a5126bddc283d90dc91265fb7b
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2014-11-24 19:50:45 -0800

    background image

--- js/main.js
+++ js/main.js
@@ -1,41 +1,39 @@
-var addButton = document.getElementById("addButton");
-var subtractButton = document.getElementById("subtractButton");
-var multiplyButton = document.getElementById("multiplyButton");
-var divideButton = document.getElementById("divideButton");
+var euroButton = document.getElementById("euroButton");
+var yenButton = document.getElementById("yenButton");
+var wonButton = document.getElementById("wonButton");
+var pesosButton = document.getElementById("pesosButton");
+var poundButton = document.getElementById("poundButton");
+
 
 var digit1 = document.getElementById("digit1");
-var digit2 = document.getElementById("digit2");
 var result = document.getElementById("result");
 
-
-addButton.addEventListener("click", function (){
+euroButton.addEventListener("click", function (){
 	a = digit1.value;
-	b = digit2.value;
-	c = a + b;
-	c = result.value;
-	console.log(c);
+	b = a * 0.80;
+	result.value = b;
 });
 
-subtractButton.addEventListener("click", function(){
+yenButton.addEventListener("click", function(){
 	a = digit1.value;
-	b = digit2.value;
-	c = a - b;
-	c = result.value;
-	console.log(c);
+	b = a * 118.10;
+	result.value = b;
 });
 
-multiplyButton.addEventListener("click", function(){
+wonButton.addEventListener("click", function(){
 	a = digit1.value;
-	b = digit2.value;
-	c = a * b;
-	c = result.value;
-	console.log(c);
+	b = a * 1114.90;
+	result.value = b;
 });
 
-divideButton.addEventListener("click", function(){
+pesosButton.addEventListener("click", function(){
 	a = digit1.value;
-	b = digit2.value;
-	c = a / b;
-	c = result.value;
-	console.log(c);
+	b = a * 13.66;
+	result.value = b;
+});
+
+poundButton.addEventListener("click", function(){
+	a = digit1.value;
+	b = a * 0.64;
+	result.value = b;
 });
